0ee23c5c0b4c04dcd2e8c8336ca8b55b58eb7d49
Parl4224h <wparkerh@comcast.net>
2022-03-11 08:45:10 -0600

    Initial Commit

---
+++ Simple Math (SwiftUI)/Simple_Math__SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_Math__SwiftUI_App.swift
+//  Simple Math (SwiftUI)
+//
+//  Created by Parker Hoffmann on 3/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Simple_Math__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
